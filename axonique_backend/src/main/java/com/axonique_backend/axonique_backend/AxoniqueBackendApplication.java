f8353db32919e4c6427d935255b4a913a0bbc5f2
Mitheja Manoharie <mmanoharie@gmail.com>
2026-02-22 11:50:54 +0530

    Configured both backend and frontend apps

---
+++ axonique_backend/src/main/java/com/axonique_backend/axonique_backend/AxoniqueBackendApplication.java
@@ -0,0 +1,13 @@
+package com.axonique_backend.axonique_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxoniqueBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxoniqueBackendApplication.class, args);
+	}
+
+}
---
+++ axonique_backend/src/test/java/com/axonique_backend/axonique_backend/AxoniqueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axonique_backend.axonique_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxoniqueBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
